a227f8775aff2222dd564d5e994cd84401041e3e
Igor de Souza <soujigor@gmail.com>
2021-12-15 17:51:31 -0300

    adding smooth transition to navlinks

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -23,16 +23,52 @@ const Navbar = ({ toggle }) => {
           </MobileIcon>
           <NavMenu>
             <NavItem>
-              <NavLinks to="sobre">Sobre</NavLinks>
+              <NavLinks
+                to="sobre"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                Sobre
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="fotos">Fotos</NavLinks>
+              <NavLinks
+                to="fotos"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                Fotos
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="depoimentos">Depoimentos</NavLinks>
+              <NavLinks
+                to="depoimentos"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                Depoimentos
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="tutores">Tutores</NavLinks>
+              <NavLinks
+                to="tutores"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                Tutores
+              </NavLinks>
             </NavItem>
           </NavMenu>
           <NavBtn>
